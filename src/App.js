eaa7e9773b9b02e7ab829278da9e1e090db77147
Mohamed Amn <mamn2@illinois.edu>
2019-09-19 22:54:04 -0500

    fixed scaling bugs and added a little phono support

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ function App() {
         <Header className='header-color' title={<Link style={{textDecoration: 'none', color:'white'}} to="/">My Portfolio</Link>} scroll>
             <Navigation>
                 <Link to="/projects">Projects</Link>
-                <a href={Resume} target="_blank">Resume</a>
+                <a href={Resume} rel ="noopener noreferrer" target="_blank">Resume</a>
                 <Link to="/aboutme">About</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+            <Navigation>
+              <Link to="/projects">Projects</Link>
+              <a href={Resume} rel ="noopener noreferrer" target="_blank">Resume</a>
+              <Link to="/aboutme">About</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
         <Content>
             <div className="page-content" />
             <Main/>
--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
-import JuchePic from './photos/JucheFull.jpg';
-import FlumeGorge from './photos/FlumeGorge.jpg';
+import React, { Component } from 'react'
 import FlumeGorge2 from './photos/FlumeGorge2.jpg';
 import HungarianPic from './photos/HungarianSquare.png';
 import { Grid, Cell } from 'react-mdl';
-import BusinessPic from './photos/Business Pic 2 Copy.jpg';
 
 
 class About extends Component {
@@ -12,10 +9,9 @@ class About extends Component {
     return(
       <div className='about-body'>
         <Grid className='about-grid'>
-          <Cell col={6}>
+          <Cell style={{width: '40%', height: '80%'}} col={6}  className='parent'>
             <h3>About Me</h3>
-            <p style={{width: '75%', margin: 'auto', paddingTop: '0em'}}>
-              Wow! A whole page to write about me?? This is going to be fun!<br></br><br></br>
+            <p className='child'>
               Hey there, I'm Mohamed, an enthusiastic musician, traveller, and software engineer.
               I'm so glad to see that you've stumbled upon my page!
               As you may know, I am a student at UIUC, majoring in computer science and music. That's right,
@@ -34,41 +30,17 @@ class About extends Component {
 
             <div className='pics'>
 
-            <img
-              className='aboutme-pic1'
-              src={HungarianPic}
-              alt="avatar"
-            />
-            <img
-              className='aboutme-pic2'
-              src={FlumeGorge2}
-              alt="avatar"
-            />
-
-              {/*<List>
-
-                <ListItem>
-                  <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton',}}>
-                    <i className="fa fa-phone-square" aria-hidden="true"/>
-                    1 (331) 643 6832
-                  </ListItemContent>
-                </ListItem>
-
-                <ListItem>
-                  <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton',}}>
-                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
-                    /in/mohamedamn
-                  </ListItemContent>
-                </ListItem>
-
-                <ListItem>
-                  <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton',}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                    mamn2@illinois.edu
-                  </ListItemContent>
-                </ListItem>
-
-              </List>*/}
+              <img
+                className='aboutme-pic1'
+                src={HungarianPic}
+                alt="avatar"
+              />
+              <hr></hr>
+              <img
+                className='aboutme-pic2'
+                src={FlumeGorge2}
+                alt="avatar"
+              />
 
             </div>
 
